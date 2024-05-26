c07952df6df5b4dabbd17bcfc085022cacb84df2
Alex Chan <alex@alexwlchan.net>
2024-05-26 14:45:55 +0100

    Allow saving multiple books at once

--- save_book.py
+++ save_book.py
@@ -24,7 +24,6 @@ class BookInfo(typing.TypedDict):
     author: str
     series: str | None
     publication_date: datetime.date
-    format: Format
 
 
 class Dimensions(typing.TypedDict):
@@ -33,6 +32,7 @@ class Dimensions(typing.TypedDict):
 
 
 class Book(BookInfo):
+    format: Format
     filename: str
     thumbnail: str
     thumbnail_dimensions: Dimensions
@@ -47,7 +47,7 @@ def get_format(path: pathlib.Path) -> Format:
     elif path.suffix == ".pdf":
         return "PDF"
     else:
-        raise ValueError(f"Unrecognised path suffix! {path.suffix!r}")
+        raise ValueError(f"Unrecognised path suffix! {path.suffix!r} ({path})")
 
 
 def get_thumbnail(path: pathlib.Path) -> pathlib.Path:
@@ -85,16 +85,14 @@ def get_tint_color(thumbnail: pathlib.Path) -> str:
     return output.strip().decode("utf8")
 
 
-def ask_for_book(path: pathlib.Path) -> Book:
+def ask_for_book() -> Book:
     title = input("What’s the title of the book?\n> ")
     print("")
     author = input("Who’s the author?\n> ")
     print("")
-    series = input("Is the book part of a series?\n> ") or None
-    print("")
     publication_date = datetime.datetime.strptime(input("When was the book published?\n> "), "%Y-%m-%d").date()
-
-    format = get_format(path)
+    print("")
+    series = input("Is the book part of a series?\n> ") or None
 
     return {
         "title": title,
@@ -102,7 +100,6 @@ def ask_for_book(path: pathlib.Path) -> Book:
         "series": series,
         "publication_date": publication_date,
         "publication_year": publication_date.year,
-        "format": format,
     }
 
 
@@ -113,51 +110,52 @@ class DatetimeEncoder(json.JSONEncoder):
 
 
 if __name__ == "__main__":
-    try:
-        path = pathlib.Path(sys.argv[1])
-    except IndexError:
-        sys.exit(f"Usage: {__file__} PATH")
+    book_info = ask_for_book()
 
-    if not path.exists():
-        sys.exit("No such file!")
+    for path in sys.argv[1:]:
+        path = pathlib.Path(path)
 
-    book_info = ask_for_book(path)
+        if not path.exists():
+            sys.exit(f"No such file! {path}")
 
-    thumbnail_path = get_thumbnail(path)
-    thumbnail_dimensions = get_dimensions(thumbnail_path)
-    tint_color = get_tint_color(thumbnail_path)
+        format = get_format(path)
 
-    try:
-        with open(CATALOGUE_ROOT / "metadata.js") as in_file:
-            contents = in_file.read()
+        thumbnail_path = get_thumbnail(path)
+        thumbnail_dimensions = get_dimensions(thumbnail_path)
+        tint_color = get_tint_color(thumbnail_path)
 
-        existing_catalogue = json.loads(contents.strip().strip(';').replace('window.books = ', ''))
-    except FileNotFoundError:
-        existing_catalogue = []
+        try:
+            with open(CATALOGUE_ROOT / "metadata.js") as in_file:
+                contents = in_file.read()
 
-    stem = pathlib.Path(book_info['author']) / f"{book_info['title']} ({book_info['publication_date'].year})"
+            existing_catalogue = json.loads(contents.strip().strip(';').replace('window.books = ', ''))
+        except FileNotFoundError:
+            existing_catalogue = []
 
-    # Copy the file into place
-    filename = f"{stem}.{book_info['format'].lower()}"
-    assert not (CATALOGUE_ROOT / filename).exists()
-    (CATALOGUE_ROOT / filename).parent.mkdir(exist_ok=True, parents=True)
-    shutil.move(path, CATALOGUE_ROOT / filename)
+        stem = pathlib.Path(book_info['author']) / f"{book_info['title']} ({book_info['publication_date'].year})"
 
-    # Copy the thumbnail into place
-    thumbnail = pathlib.Path(".thumbnails") / f"{filename}.png"
-    assert not (CATALOGUE_ROOT / thumbnail).exists()
-    (CATALOGUE_ROOT / thumbnail).parent.mkdir(exist_ok=True, parents=True)
-    shutil.move(thumbnail_path, CATALOGUE_ROOT / thumbnail)
+        # Copy the file into place
+        filename = f"{stem}.{format.lower()}"
+        assert not (CATALOGUE_ROOT / filename).exists()
+        (CATALOGUE_ROOT / filename).parent.mkdir(exist_ok=True, parents=True)
+        shutil.move(path, CATALOGUE_ROOT / filename)
 
-    existing_catalogue.append({
-        **book_info,
-        'filename': str(filename),
-        'thumbnail': str(thumbnail),
-        'thumbnail_dimensions': thumbnail_dimensions,
-        'tint_color': tint_color,
-    })
+        # Copy the thumbnail into place
+        thumbnail = pathlib.Path(".thumbnails") / f"{filename}.png"
+        assert not (CATALOGUE_ROOT / thumbnail).exists()
+        (CATALOGUE_ROOT / thumbnail).parent.mkdir(exist_ok=True, parents=True)
+        shutil.move(thumbnail_path, CATALOGUE_ROOT / thumbnail)
 
-    js_string = f"window.books = {json.dumps(existing_catalogue, indent=2, sort_keys=True, cls=DatetimeEncoder)};\n"
+        existing_catalogue.append({
+            **book_info,
+            'format': format,
+            'filename': str(filename),
+            'thumbnail': str(thumbnail),
+            'thumbnail_dimensions': thumbnail_dimensions,
+            'tint_color': tint_color,
+        })
 
-    with open(CATALOGUE_ROOT / "metadata.js", "w") as out_file:
-        out_file.write(js_string)
+        js_string = f"window.books = {json.dumps(existing_catalogue, indent=2, sort_keys=True, cls=DatetimeEncoder)};\n"
+
+        with open(CATALOGUE_ROOT / "metadata.js", "w") as out_file:
+            out_file.write(js_string)
